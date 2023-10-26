46a0d4b698bc3b17457aedb20da4e5f6ceae498b
Dzikrullah Umam <38921980+umam123@users.noreply.github.com>
2023-10-27 03:13:20 +0700

    Update Diabetes_app.py

--- Diabetes_app.py
+++ Diabetes_app.py
@@ -17,13 +17,13 @@ with open ('KNN_pickel.pkl', 'rb') as r :
 
 st.title("Diabetes Predictor")
 
-melahirkan = st.number_input("Pregnancies", min_value=0)
+melahirkan = st.number_input("Pregnancies", min_value=0, max_value=9)
 glukosa = st.number_input("Glucose", min_value=0)
 darah = st.number_input("BloodPressure", min_value=0)
 kulit = st.number_input("SkinThickness", min_value=0)
 insulin = st.number_input("Insulin", min_value=0)
-bmi = st.number_input("BMI", min_value=0)
-riwayat = st.number_input("DiabetesPedigreeFunction", min_value=0)
+bmi = st.number_input("BMI", min_value=0.001)
+riwayat = st.number_input("DiabetesPedigreeFunction", min_value=0.001)
 umur = st.number_input("Age", min_value=0)
 
 st.text("Enter your data and click the 'Predict' button to see the result.")
