25bc052e7c2e8fb160f09916b5ec650bcdeda7fe
Dzikrullah Umam <38921980+umam123@users.noreply.github.com>
2023-10-28 00:23:18 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -5,7 +5,7 @@ import streamlit as st
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
 #judul web
-st.title('Diabetes Predictor')
+st.markdown("# <center>Diabetes Predictor</center>", unsafe_allow_html=True)
 
 #membagi kolom
 col1, col2 = st.columns(2)
