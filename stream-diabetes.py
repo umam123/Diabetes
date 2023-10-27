ce455a4c9ba9b6128b1cea594b04dc24dd14773c
Dzikrullah Umam <38921980+umam123@users.noreply.github.com>
2023-10-28 02:46:55 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -30,12 +30,12 @@ with col2 :
     BMI = st.number_input ("BMI", min_value=0.01)
 
 with col1 :
-    st.text("DPF = Number of family with diabetes/Total of family member")
     DiabetesPedigreeFunction = st.number_input ("Diabetes Pedigree Function (DPF)", min_value=0.001)
 
 with col2 :
     Age = st.number_input ("Age", min_value=0)
 
+st.text("DPF = Number of family with diabetes/Total of family member")
 # code untuk prediksi
 diab_diagnosis = ''
 
