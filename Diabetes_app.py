bde8d2445d33f2bf6544c8bf3c2a8b23c8d2c374
Dzikrullah Umam <38921980+umam123@users.noreply.github.com>
2023-10-27 23:41:37 +0700

    Update Diabetes_app.py

--- Diabetes_app.py
+++ Diabetes_app.py
@@ -15,16 +15,27 @@ import numpy as np
 # Load the trained model
 model = pickle.load(open('KNN_pickel.sav', 'rb'))
 
-st.title("Diabetes Predictor")
 
-melahirkan = st.number_input("Pregnancies", min_value=0, max_value=9)
-glukosa = st.number_input("Glucose", min_value=0)
-darah = st.number_input("BloodPressure", min_value=0)
-kulit = st.number_input("SkinThickness", min_value=0)
-insulin = st.number_input("Insulin", min_value=0)
-bmi = st.number_input("BMI", min_value=0.001)
-riwayat = st.number_input("DiabetesPedigreeFunction", min_value=0.001)
-umur = st.number_input("Age", min_value=0)
+
+st.title("Diabetes Predictor")
+# Membagi Kolom
+col1, col2 = st.columns (2)
+with col1 :
+    melahirkan = st.number_input("Pregnancies", min_value=0, max_value=9)
+with col2 :
+    glukosa = st.number_input("Glucose", min_value=0)
+with col1 :
+    darah = st.number_input("BloodPressure", min_value=0)
+with col2 :
+    kulit = st.number_input("SkinThickness", min_value=0)
+with col1 :
+    insulin = st.number_input("Insulin", min_value=0)
+with col2 :
+    bmi = st.number_input("BMI", min_value=0.001)
+with col1 :
+    riwayat = st.number_input("DiabetesPedigreeFunction", min_value=0.001)
+with col2 :
+    umur = st.number_input("Age", min_value=0)
 
 st.text("Enter your data and click the 'Predict' button to see the result.")
 if st.button("Predict"):
