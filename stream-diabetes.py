9ca03fd4bf8c6521374de17cf7c1ed9d41d5eb10
Dzikrullah Umam <38921980+umam123@users.noreply.github.com>
2023-10-28 02:53:13 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -41,14 +41,7 @@ diab_diagnosis = ''
 
 
 # membuat tombol untuk prediksi
-if st.markdown(
-    """
-    <div style="display: flex; justify-content: center; align-items: center;">
-        <button style="padding: 10px 20px;">Predict</button>
-    </div>
-    """,
-    unsafe_allow_html=True
-):
+if st.button("Predict"):
     data = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]]).reshape(1, -1)
     diab_prediction = diabetes_model.predict(data)
 
