88bac6cb0d779bb08d83cb68b3aa813a3e47e8c9
Dzikrullah Umam <38921980+umam123@users.noreply.github.com>
2023-10-28 11:14:01 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -45,7 +45,6 @@ with col4 :
     if st.button("Predict"):
         data = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]]).reshape(1, -1)
         diab_prediction = diabetes_model.predict(data)
-     
         if diab_prediction[0] == 1:
             st.markdown(f"<h1 style='text-align: center; font-size: {font_size}px;'>The model predicts that you have diabetes.</h1>", unsafe_allow_html=True)
         else:
